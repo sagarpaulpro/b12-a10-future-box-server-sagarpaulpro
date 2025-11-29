7607b0e25bcb2c5035f2d919c53770d69ac325e9
Sagar <sagarpaul01@yahoo.com>
2025-11-29 19:59:19 +0600

    Added API get

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors())
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@plateshare.fj8ri9v.mongodb.net/?appName=PlateShare`;
 
-app.get('/', (req , res)=>{
-    res.send('Hello World!')
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Server is running.');
 })
 
-app.listen(port, ()=>{
-    console.log(`Example app listening on port ${port}`)
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('PlateShare');
+    const foods = database.collection('foods');
+
+    app.post('/addfood', async (req, res) => {
+      const query = req.body;
+      const result = await foods.insertOne(query);
+      console.log(result);
+      res.send();
+    } )
+    app.get('/food', async (req, res)=>{
+      const result = await foods.find().toArray();
+      res.send(result)
+    })
+}
+  finally {
+
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`)
 })
